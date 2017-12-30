fb58f88da425d80bd61b0f979e92eb7a829ea8db
Tim Kendall <timkendall18@gmail.com>
2017-12-30 10:38:18 -0800

    Move Crynamo::Operation enum into AWS::DynamoDB

--- src/aws/dynamodb.cr
+++ src/aws/dynamodb.cr
@@ -1,4 +1,11 @@
 require "./dynamodb/**"
 
 module AWS::DynamoDB
+  # Represents the supported AWS DynamoDB operations (note: this is not exhaustive).
+  enum Operation
+    GetItem
+    PutItem
+    UpdateItem
+    DeleteItem
+  end
 end
